8575df40f9f217803253f39fa1eff55d9588b693
Antony Chavez <ivanchr2199@outlook.com>
2021-06-08 10:45:07 -0500

    ADD: GUI completed

--- main.py
+++ main.py
@@ -29,7 +29,6 @@ class main():
         self.line_styles   = list(line_style.keys())
         self.marker_style  = list(marker_style.keys())
 
-        self.padd_y = 5
         # Window container
         self.root = Tk()
         self.root.title("Graph")
@@ -38,23 +37,23 @@ class main():
         self.root.resizable(height = None, width = None)
         #Contains the graph configuration that user set
         self.config_graph_frame =Frame(self.root, borderwidth = 1)
-        self.config_graph_frame.pack(side = LEFT,pady=self.padd_y) 
+        self.config_graph_frame.pack(side = LEFT,pady=5) 
 
         self.conf_container=Frame(self.config_graph_frame)
         self.conf_container.pack()
 
-        #labels container
-        self.text_frame = Frame(self.conf_container)
-        self.text_frame.pack(side = LEFT)
-
         #input boxes container
         self.input_frame = Frame(self.conf_container)
-        self.input_frame.pack()
+        self.input_frame.pack(side = RIGHT)
+        #labels container
+        self.text_frame = Frame(self.conf_container)
+        self.text_frame.pack()
 
+        
         #-----------------------------------------------------------
 
         self.path_label = Label(self.text_frame, text="File data: ")
-        self.path_label.pack(pady=self.padd_y, anchor ="e")
+        self.path_label.pack(pady=5, anchor ="e")
 
         self.import_data_Btn = Button(
             self.input_frame, text=". . .", command=self.import_data)
@@ -63,13 +62,12 @@ class main():
         self.path = StringVar()
         self.path_box = Entry(self.input_frame,
                               textvariable=self.path, width=15)
-        self.path_box.config( )
-        self.path_box.pack(pady=self.padd_y)
+        self.path_box.pack(pady=5)
         # Selecti file button
         
         # title
         self.title_label = Label(self.text_frame, text="Title: ")
-        self.title_label.pack(pady=self.padd_y, anchor ="e")
+        self.title_label.pack(pady=5, anchor ="e")
 
         # titulo de la grafica
         self.title = StringVar()
@@ -77,71 +75,68 @@ class main():
         self.title_box = Entry(self.input_frame,
                                textvariable=self.title, width=15)
         
-        self.title_box.pack(pady=self.padd_y)
+        self.title_box.pack(pady=5)
 
         #nombre del eje y
         self.x_label = Label(self.text_frame, text="X-axis title:")
-        self.x_label.pack(pady=self.padd_y, anchor ="e")
+        self.x_label.pack(pady=5, anchor ="e")
 
         self.x_name = StringVar()
         self.x_name.set("name (units)")
         self.x_name_box = Entry(self.input_frame,
                                 textvariable=self.x_name, width=15)
-        self.x_name_box.config( )
-        self.x_name_box.pack(pady=self.padd_y)
+        self.x_name_box.pack(pady=5)
 
         #nombre del eje x
         self.y_label = Label(self.text_frame, text="Y-axis title:")
-        self.y_label.pack(pady=self.padd_y, anchor ="e")
+        self.y_label.pack(pady=5, anchor ="e")
         
         
         self.y_name = StringVar()
         self.y_name.set("name (units)")
         self.y_name_box = Entry(self.input_frame,
                                 textvariable=self.y_name, width=15)
-        self.y_name_box.config( )
-        self.y_name_box.pack(pady=self.padd_y)
+        self.y_name_box.pack(pady=5)
 
         #nombre de columna
         self.x_name_variable_label = Label(self.text_frame, text="X: ")
-        self.x_name_variable_label.pack(pady=self.padd_y, anchor ="e")
+        self.x_name_variable_label.pack(pady=5, anchor ="e")
 
         self.x_data = ttk.Combobox(self.input_frame, width=15)
-        self.x_data.pack(pady=self.padd_y)
+        self.x_data.pack(pady=5)
         self.x_data['values'] = self.title_columns
         
         #nombre de columna
         self.y_name_variable_label = Label(self.text_frame, text="Y: ")
-        self.y_name_variable_label.pack(pady=self.padd_y, anchor ="e")
+        self.y_name_variable_label.pack(pady=5, anchor ="e")
 
         self.y_data = ttk.Combobox(self.input_frame, width=15)
-        self.y_data.pack(pady=self.padd_y)
+        self.y_data.pack(pady=5)
         self.y_data['values'] = self.title_columns
 
         #ecuacion de la grafica
         self.equation_label = Label(self.text_frame, text="Equation")
-        self.equation_label.pack(pady=self.padd_y, anchor ="e")
+        self.equation_label.pack(pady=5, anchor ="e")
 
         self.equation = StringVar()
         self.equation.set("x**2")
         self.equation_box = Entry(
             self.input_frame, textvariable=self.equation, width=15)
-        self.equation_box.pack(pady=self.padd_y)
+        self.equation_box.pack(pady=5)
 
         #dominio de la ecuacion
         self.domain_label = Label(self.text_frame, text="Domain a:b")
-        self.domain_label.pack(pady=self.padd_y, anchor ="e")
+        self.domain_label.pack(pady=5, anchor ="e")
 
         self.domain = StringVar()
         self.domain.set("")
         self.domain_box = Entry(self.input_frame,
                                 textvariable=self.domain, width=15)
-        self.domain_box.config( )
-        self.domain_box.pack(pady=self.padd_y)
+        self.domain_box.pack(pady=5)
 
         #legenda de la grafica
         self.legend_label = Label(self.text_frame, text="Legend")
-        self.legend_label.pack(pady=self.padd_y, anchor ="e")
+        self.legend_label.pack(pady=5, anchor ="e")
 
         self.legend_equation = StringVar()
         self.legend_equation.set("")
@@ -149,17 +144,34 @@ class main():
             self.input_frame, textvariable=self.legend_equation, width=15)
         self.legend_equation_box.pack(pady = 5)
 
+        # color de la curva de la ecuacion
         self.color_curve_label = Label(self.text_frame, text="Color de curva: ")
-        self.color_curve_label.pack(pady=self.padd_y, anchor ="e")
+        self.color_curve_label.pack(pady=5, anchor ="e")
 
         self.color_curve = "#1F77B4"
         self.color_equation_button = Button(
             self.input_frame, bg=self.color_curve, width=1, command=self.select_colour_equation)
-        self.color_equation_button.pack(pady=self.padd_y, anchor = "sw")
+        self.color_equation_button.pack(anchor = "sw", pady = 5)
 
-        #contenido del frame de estilos ------------------------------------
+        
+        #color de los puntos de los datos
+        self.color_label = Label(self.text_frame, text="Color Points:")
+        self.color_label.pack(pady=5)
+        self.color_points = "#FF0000"
+        self.color_button = Button(self.input_frame,
+                                   bg=self.color_points, width=1,command=self.select_colour_points)
+        self.color_button.pack(anchor ="w", pady = 5)
+
+        self.marker_label = Label(self.text_frame, text="Marker: ")
+        self.marker_label.pack(pady=5, anchor ="e")
+
+        self.marker_list = ttk.Combobox(self.input_frame, width=15)
+        self.marker_list.pack(pady=5)
+        self.marker_list['values'] = self.marker_style
+
+        #plot grid 
         self.grid_label = Label(self.text_frame, text = "grid")
-        self.grid_label.pack(pady=self.padd_y, anchor ="e") 
+        self.grid_label.pack(pady=5, anchor ="e") 
 
        
         self.grid_value = BooleanVar()
@@ -170,41 +182,28 @@ class main():
 				    offvalue=False,
 				    height=1,
 				    width=1)
-        self.grid.pack(pady=self.padd_y, anchor = "sw")
+        self.grid.pack(anchor = "sw")
 
-        self.color_label = Label(self.text_frame, text="Color Points:")
-        self.color_label.pack(pady=self.padd_y)
-        self.color_points = "#FF0000"
-        self.color_button = Button(self.input_frame,
-                                   bg=self.color_points,width=1, command=self.select_colour_points)
-        self.color_button.pack(pady=self.padd_y, anchor ="w")
-
-        self.marker_label = Label(self.text_frame, text="Marker: ")
-        self.marker_label.pack(pady=self.padd_y, anchor ="e")
-
-        self.marker_list = ttk.Combobox(self.input_frame, width=15)
-        self.marker_list.pack(pady=self.padd_y)
-        self.marker_list['values'] = self.marker_style
 
         # Lyne styles
         self.line_style_label = Label(
             self.text_frame, text="Line Style: ")
-        self.line_style_label.pack(pady=self.padd_y, anchor ="e")
+        self.line_style_label.pack(pady=5, anchor ="e")
 
         self.line_style = ttk.Combobox(self.input_frame, width=15)
-        self.line_style.pack(pady=self.padd_y)
+        self.line_style.pack(pady=5)
         self.line_style['values'] = self.line_styles
 
         # Size Points
         self.size_label = Label(self.text_frame, text="Points size: ")
-        self.size_label.pack(pady=self.padd_y, anchor ="e")
+        self.size_label.pack(pady=5, anchor ="e")
         # Caja de entrada para el tamaño de los puntos
         self.size_var = DoubleVar()
         self.size_var.set(5.0)
         self.size_point = Spinbox(self.input_frame, from_=0.1,
                                   increment=0.1, to=5.0, width=7, textvariable=str(self.size_var))
 
-        self.size_point.pack(pady=self.padd_y)
+        self.size_point.pack(pady=5)
 
         
         #-----------------------------------------------------
@@ -235,8 +234,17 @@ class main():
         self.root.mainloop()
 
     def resize(self, event):
-        pass
-    
+        if self.x_resolution == event.width:
+            for label_el in self.text_frame.winfo_children():
+                label_el.configure(pady = 5)
+
+            for input_el in self.input_frame.winfo_children():
+                if input_el != self.color_button:
+                    print("sii")
+                    input_el.pack(pady = 10)
+            self.grid.pack(pady = 1)
+
+
     def import_data(self):
 
         self.path_file = filedialog.askopenfilename(initialdir="./", title="Select file", 
